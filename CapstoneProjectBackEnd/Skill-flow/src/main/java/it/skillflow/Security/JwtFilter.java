0aad4cb2e45e93a6058b1b79d3f21fc23da625b1
Simonecaste96 <simonecaste96@gmail.com>
2024-07-06 19:43:12 +0200

    Modificati file properties e specificata versione Java per deploy con Koyeb

--- CapstoneProjectBackEnd/Skill-flow/src/main/java/it/skillflow/Security/JwtFilter.java
+++ CapstoneProjectBackEnd/Skill-flow/src/main/java/it/skillflow/Security/JwtFilter.java
@@ -33,7 +33,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
