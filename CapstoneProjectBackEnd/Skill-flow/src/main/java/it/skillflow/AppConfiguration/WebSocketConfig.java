115baee0bc2e11fa0b4033e1c83d6b68f77575b2
Simonecaste96 <simonecaste96@gmail.com>
2024-07-06 18:54:11 +0200

    Modificati file properties e specificata versione Java per deploy con Koyeb

--- CapstoneProjectBackEnd/Skill-flow/src/main/java/it/skillflow/AppConfiguration/WebSocketConfig.java
+++ CapstoneProjectBackEnd/Skill-flow/src/main/java/it/skillflow/AppConfiguration/WebSocketConfig.java
@@ -29,7 +29,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins(allowedOrigins.split(",")).withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns(allowedOrigins.split(",")).withSockJS();
     }
 /*
     @Override
